d86f47752c3eedcea4af8ab9e70e00c9d7780197
Benito Lopez <bennobenno2730@gmail.com>
2023-07-29 00:28:36 -0600

    Agregando paquetes faltantes para renderizacion

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 //jshint esversion:6
+const express=require("express");
+const bodyParser=require("body-parser");
+const ejs=require("ejs");
+
+
+const app=express()
+app.use(express.static("public"));
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended:true}))
+
+
+app.get("/",(req,res)=>{
+    res.render("home.ejs")
+})
+app.get("/register",(req,res)=>{
+        res.render("register.ejs")
+})
+
+
+
+app.listen(3000,function(){
+    console.log("Servidor inicializado en el port 3000")
+})
