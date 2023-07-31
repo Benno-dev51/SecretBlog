ca887ddda62cf6fc2bf1af6a2e15d3b971125553
Benito Lopez <bennobenno2730@gmail.com>
2023-07-30 20:37:17 -0600

    agregando variables de entorno y modificando el ignore

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
 //jshint esversion:6
+require('dotenv').config()
 const express=require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const app=express()
+const encrypt=require("mongoose-encryption")
 const mongoose=require("mongoose");
+
 app.use(express.static("public"));
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}))
 
 mongoose.connect("mongodb://127.0.0.1:27017/userDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
-const userSchema={
+const userSchema= new mongoose.Schema({
     email:String,
     password:String,
-}
+})
+
+
+userSchema.plugin(encrypt, { secret:process.env.SECRET,encryptedFields: ['password']});
 const User= new mongoose.model("User",userSchema)
-
-
-
-
 
 app.get("/",(req,res)=>{
     res.render("home.ejs")
@@ -50,7 +52,7 @@ app.post("/login",(req,res)=>{
     const username=req.body.username;
     const password=req.body.password;
 
-    
+
     User.findOne({ email: username })
     .then((foundUser) => {
         if (foundUser) {
@@ -66,7 +68,6 @@ app.post("/login",(req,res)=>{
     .catch((err) => {
         console.log(err);
     });
-
 
 })
 
